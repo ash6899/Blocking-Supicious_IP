61607b17281790bf19920af7f4d73e60647e44d9
Yash Khandelwal <yashkhandelwal2017@gmail.com>
2020-07-01 06:33:15 +0530

    Update blocked-IP.py

--- blocked-IP.py
+++ blocked-IP.py
@@ -2,4 +2,4 @@ import os
 with open("/root/mlops-task-5/blocked.txt","r") as file1:
 	for i in file1:
 		os.system('iptables -A INPUT -s {} -j DROP'.format(i.rstrip('\n')))
-	os.system('service iptables save')
+	os.system('systemctl iptables save')
